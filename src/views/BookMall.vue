ede3e36fd834f65223f03b77e622bea87b969f7a
Your Name <email@example.com>
2020-04-24 15:08:54 +0800

    页面搭建

--- src/views/BookMall.vue
+++ src/views/BookMall.vue
@@ -8,8 +8,8 @@
     </van-swipe>
     <van-grid :column-num="3" :border="false" clickable>
       <van-grid-item icon="apps-o" text="分类" to="/classification" />
-      <van-grid-item icon="send-gift-o" text="免费" to="/free"/>
-      <van-grid-item icon="sign" text="完结" to="/completed"/>
+      <van-grid-item icon="send-gift-o" text="免费" to="/free" />
+      <van-grid-item icon="sign" text="完结" to="/completed" />
     </van-grid>
     <van-pull-refresh v-model="refreshing" @refresh="onRefresh">
       <van-list
@@ -18,8 +18,82 @@
         finished-text="我也是有底线的"
         @load="onLoad"
       >
-        <li></li>
-        <van-cell v-for="item in list" :key="item" :title="item" />
+        <li>
+          <div class="list-header">
+            <span class="header-left">精品汇聚</span>
+            <div class="header-right">
+              <span>换一换</span>
+              <van-icon name="replay" @click="change()" />
+            </div>
+          </div>
+          <div class="bookmall-lists">
+            <div class="bookmall-list" @click="bookDetail()">
+              <img
+                src="http://img-tailor.11222.cn/bcv/big/1170156568764.jpg"
+                alt="加载中"
+              />
+              <span class="booktitle">太古龙尊</span>
+              <span class="bookautho">天蚕土豆</span>
+            </div>
+            <div class="bookmall-list" @click="bookDetail()">
+              <img
+                src="http://img-tailor.11222.cn/bcv/big/1170156568764.jpg"
+                alt="加载中"
+              />
+              <span class="booktitle">太古龙尊</span>
+              <span class="bookautho">天蚕土豆</span>
+            </div>
+            <div class="bookmall-list" @click="bookDetail()">
+              <img
+                src="http://img-tailor.11222.cn/bcv/big/1170156568764.jpg"
+                alt="加载中"
+              />
+              <span class="booktitle">太古龙尊</span>
+              <span class="bookautho">天蚕土豆</span>
+            </div>
+            <div class="bookmall-list" @click="bookDetail()">
+              <img
+                src="http://img-tailor.11222.cn/bcv/big/1170156568764.jpg"
+                alt="加载中"
+              />
+              <span class="booktitle">太古龙尊</span>
+              <span class="bookautho">天蚕土豆</span>
+            </div>
+            <div class="bookmall-list" @click="bookDetail()">
+              <img
+                src="http://img-tailor.11222.cn/bcv/big/1170156568764.jpg"
+                alt="加载中"
+              />
+              <span class="booktitle">太古龙尊</span>
+              <span class="bookautho">天蚕土豆</span>
+            </div>
+            <div class="bookmall-list" @click="bookDetail()">
+              <img
+                src="http://img-tailor.11222.cn/bcv/big/1170156568764.jpg"
+                alt="加载中"
+              />
+              <span class="booktitle">太古龙尊</span>
+              <span class="bookautho">天蚕土豆</span>
+            </div>
+            <div class="bookmall-list" @click="bookDetail()">
+              <img
+                src="http://img-tailor.11222.cn/bcv/big/1170156568764.jpg"
+                alt="加载中"
+              />
+              <span class="booktitle">太古龙尊</span>
+              <span class="bookautho">天蚕土豆</span>
+            </div>
+            <div class="bookmall-list" @click="bookDetail()">
+              <img
+                src="http://img-tailor.11222.cn/bcv/big/1170156568764.jpg"
+                alt="加载中"
+              />
+              <span class="booktitle">太古龙尊</span>
+              <span class="bookautho">天蚕土豆</span>
+            </div>
+          </div>
+        </li>
+        <!-- <van-cell v-for="item in list" :key="item" :title="item" /> -->
       </van-list>
     </van-pull-refresh>
     <Footer />
@@ -54,6 +128,7 @@ export default {
     };
   },
   methods: {
+    change() {},
     onLoad() {
       setTimeout(() => {
         if (this.refreshing) {
@@ -105,8 +180,9 @@ export default {
     overflow: hidden;
     -webkit-user-select: none;
     user-select: none;
-    width: 100%;
     height: 160px;
+    width: 92%;
+    margin: 0 4%;
     .van-swipe__track img {
       width: 100%;
       height: 160px;
@@ -120,6 +196,60 @@ export default {
     display: flex;
     -webkit-flex-wrap: wrap;
     flex-wrap: wrap;
+  }
+  .van-pull-refresh {
+    width: 100%;
+    overflow: hidden;
+    -webkit-user-select: none;
+    user-select: none;
+    padding: 0 10px;
+    .list-header {
+      // height: 10px;
+      width: 100%;
+      display: flex;
+      justify-content: space-between;
+      align-items: center;
+      .header-left {
+        margin-left: 8px;
+      }
+      .header-left::before {
+        content: "";
+        width: 0.2rem;
+        height: 1.2rem;
+        display: inline-block;
+        background: #00c98d;
+        position: absolute;
+        top: 0.09rem;
+        left: 0px;
+      }
+      .header-right {
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        width: 70px;
+      }
+    }
+    .bookmall-lists {
+      width: 100%;
+      display: flex;
+      flex-wrap: wrap;
+      justify-content: flex-start;
+      .bookmall-list {
+        width: 20%;
+        display: flex;
+        flex-direction: column;
+        padding: 20px 0 0 4%;
+        img {
+          width: 100%;
+          height: 100px;
+        }
+        .bookautho {
+          font-size: 0.8rem;
+          color: #000;
+          opacity: 0.5;
+        }
+      }
+    }
   }
 }
 </style>
--- src/views/Profile.vue
+++ src/views/Profile.vue
@@ -1,6 +1,20 @@
 <template>
   <div id="profile">
     <van-nav-bar fixed title="个人中心" />
+    <div class="profile-header">
+      <van-image
+        round
+        width="4rem"
+        height="4rem"
+        src="https://img.yzcdn.cn/vant/cat.jpeg"
+      />
+      <span class="profile-login" @click="login()">登录/注册</span>
+    </div>
+    <van-divider />
+    <van-cell title="消息" is-link icon="chat-o" to="/message" />
+    <van-cell title="收藏" is-link icon="star-o" to="/collection" />
+    <van-cell title="书评" is-link icon="comment-o" to="/bookcomments" />
+    <van-cell title="意见反馈" is-link icon="comment-o" to="/bookcomments" />
     <Footer />
   </div>
 </template>
@@ -10,6 +24,11 @@ import Footer from "../components/Footer";
 export default {
   components: {
     Footer,
+  },
+  methods: {
+    login() {
+      this.$router.push("/login");
+    },
   },
 };
 </script>
@@ -29,6 +48,19 @@ export default {
     color: #323233;
     font-weight: 600;
     font-size: 16px;
+  }
+  .profile-header {
+    display: flex;
+    justify-content: flex-start;
+    align-items: center;
+    margin: 20px 0 20px 10px;
+  }
+  .van-image {
+    margin-right: 10px;
+  }
+  .van-cell__left-icon {
+    margin-right: 5px;
+    font-size: 25px;
   }
 }
 </style>
